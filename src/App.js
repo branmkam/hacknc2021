e62d8687281ab7f65849f4679afdce273429460f
branmkam <54331071+branmkam@users.noreply.github.com>
2021-11-06 13:39:35 -0400

    auto check for completed is going

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-function App() {
-  const assembly = axios.create({
-    baseURL: "https://api.assemblyai.com/v2",
-    headers: {
-      authorization: "532af0971fd34ae2a954649668cf79a1",
-      "content-type": "application/json",
-    },
-  });
-  
-  assembly
+window.most_recent_id = null;
+window.status = 'not started';
+const assembly = axios.create({
+  baseURL: "https://api.assemblyai.com/v2",
+  headers: {
+    authorization: "532af0971fd34ae2a954649668cf79a1",
+    "content-type": "application/json",
+  },
+});
+
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+export async function checkIDStaggered() {
+  console.log(window.status);
+  setInterval(checkID, 5000);
+  let end = checkID(window.most_recent_id);
+  if(window.status === 'completed'){clearInterval(checkID)}
+
+}
+
+export async function getAndCheckID() {
+
+  let transcript_resp = await assembly
     .post(`/transcript`, {
-      audio_url: "https://cdn.assemblyai.com/upload/ccbbbfaf-f319-4455-9556-272d48faaf7f"
+      audio_url: "https://s3-us-west-2.amazonaws.com/blog.assemblyai.com/audio/8-7-2018-post/7510.mp3"
     })
-    .then((res) => console.log(res.data))
     .catch((err) => console.error(err));
+    
+    window.most_recent_id = transcript_resp.data.id;
+    console.log('getting new id');
+
+    checkIDStaggered(window.most_recent_id);
+}
+
+async function checkID(id) { 
+
+  console.log(id)
+  let end_data = await assembly
+  .get(`/transcript/${id}`)
+  .then((e) => {   
+    window.status = e.data.status;
+    if(e.data.status === 'completed') {
+      console.log(e.data.text);
+      let testtext = document.getElementById('testtext');
+      testtext.innerHTML = e.data.words.map(word => `${word['text']} ${word['start']/1000}-${word['end']/1000}`).join('\n');
+    }
+  })
+  .catch((err) => console.error(err));
+  // window.status = end_data.data.status;
+  console.log(window.status);
+
+  return end_data;
+}
+
+function App() {
   return (
     <div className="App">
-      testing
+      <p id="testtext">e</p>
+      <button onClick={getAndCheckID}>get id</button>
     </div>
   );
 }
